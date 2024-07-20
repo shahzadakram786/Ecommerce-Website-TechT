d0e718d0daa04e7cbea0999150858fcd4a7e0a31
shahzadakram786 <alyakram786@gmail.com>
2024-07-20 23:58:01 +0500

    d

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-// import React from "react";
-// import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
-// import Home from "./Home";
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />{" "}
-//         <Route path="/about" element={<About />} />{" "}
-//       </Routes>{" "}
-//     </Router>
-//   );
-// };
+import React from "react";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />{" "}
+        <Route path="/about" element={<About />} />{" "}
+      </Routes>{" "}
+    </Router>
+  );
+};
 
-// export default App;
+export default App;
