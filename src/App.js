7d105c9282b890716d60f86503f0d152eca2fc65
shahzadakram786 <alyakram786@gmail.com>
2024-07-29 14:09:56 +0500

    wokring on the theme veriable and implementing on home

--- src/App.js
+++ src/App.js
@@ -11,9 +11,26 @@ import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 
 const App = () => {
-  const theme = {};
+  const theme = {
+    colors: {
+      bg: "#f6f8fa",
+      footer_bg: "#0a1435",
+      btn: "rgb(98 , 84 , 243)",
+      border: "rgb(98 , 84 , 243 , 0.5)",
+      hr: "#ffffffff",
+      gradient:
+        "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252)100%)",
+      shadow:
+        "rgba(0,0,0,0.02) 0px 1px 3px 0px , rgba(27,31, 35,0.15) 0px 0px 0px 1px ",
+      shadowSupport: "rgba(0,0,0,0.16) 0px 1px 4px",
+    },
+    media: {
+      mobile: "768px",
+      tab: "998px",
+    },
+  };
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <Router>
         <GlobalStyle />
         <Routes>
@@ -25,7 +42,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />{" "}
           <Route path="*" element={<Error />} />{" "}
         </Routes>{" "}
-      </Router>
+      </Router>{" "}
     </ThemeProvider>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div> Home </div>;
+  return <Wrapper className="test"> this is Home page </Wrapper>;
 };
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.bg};
+  width: 20rem;
+  height: 20rem;
+`;
 
 export default Home;
