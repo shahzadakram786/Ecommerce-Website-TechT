28cc0cfa535e0aec144dbeda7de144ea58eda8c8
shahzadakram786 <alyakram786@gmail.com>
2024-07-30 15:45:51 +0500

    working on cart stylings

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Nav = () => {
   // this is  for  styling of the navlink
@@ -17,8 +18,46 @@ const Nav = () => {
           text-decoration: none;
           font-size: 1.8rem;
           font-weight: 500;
+          text-tranform: uppercase;
+          color: ${({ theme }) => theme.colors.black};
+          transition: color 0.3s ease-in-out;
         }
-      }
+
+&:hover,
+&:active {
+color: ${({ theme }) => theme.colors.helper};
+
+}
+
+// navbar-link {
+// font-size:4.2rem;
+// }.
+
+}
+
+
+
+.cart-trolley--link{
+ position:relative;
+
+ .cart-trolley {
+ position:relative;
+ font-size : 5.2rem;
+ }
+
+.cart-total--item{
+width:2.4rem;
+height:2.4rem;
+position:absolute;
+background-color:#000;
+color:#000;
+border-radius:50%;
+display:grid;
+place-items:center;
+top:-20%;
+left:70%;
+background-color:${({ theme }) => theme.colors.helper}
+}
     }
   `;
 
@@ -27,16 +66,29 @@ const Nav = () => {
       <div className="navbar">
         <ul className="navbar-lists">
           <li>
-            <NavLink to="/" className="navbar-link Home">
+            <NavLink to="/" className="navbar-link home-link">
               Home{" "}
             </NavLink>{" "}
-            <NavLink to="about" className="navbar-link About">
-              {" "}
+          </li>{" "}
+          <li>
+            <NavLink to="/products" className="navbar-link ">
+              Products{" "}
+            </NavLink>{" "}
+          </li>{" "}
+          <li>
+            <NavLink to="/about" className="navbar-link about-link">
               About{" "}
             </NavLink>{" "}
-            <NavLink to="contact" className="navbar-link Contact">
-              {" "}
+          </li>{" "}
+          <li>
+            <NavLink to="/contact" className="navbar-link contact-link">
               Contact{" "}
+            </NavLink>{" "}
+          </li>{" "}
+          <li>
+            <NavLink to="/cart" className="navbar-link cart-trolley--link">
+              <FiShoppingCart className="cart-trolley" />{" "}
+              <span className="cart-total--item"> 10 </span>{" "}
             </NavLink>{" "}
           </li>{" "}
         </ul>{" "}
