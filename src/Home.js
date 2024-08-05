2b20d2fa47ab0c37464c280992aa827e6c4294b2
shahzadakram786 <alyakram786@gmail.com>
2024-08-05 14:38:57 +0500

    working on the trusted and services componentes

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted"
 
 const Home = () => {
   const data = {
     name: "Shahzad Store"
   }
-  return <HeroSection myData={data} />;
+  return <>
+    <HeroSection myData={data} />;
+    <Trusted />
+    <Services />
+  </>
 };
 
 export default Home;
