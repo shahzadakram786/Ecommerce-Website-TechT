bbe8b71cdddb72af10d7d601243bb350f0cfb125
ShahzadAkram <alyakram786@gmail.com>
2024-09-25 15:49:02 +0500

    feed:singleProduct Data

--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -46,6 +46,25 @@ switch (action.type) {
             isError:true
         }    
 
+     case "SET_SINGLE_LOADING":
+        return {
+            ...state,
+            isSingleLoading:true
+        }   
+
+     case "SET_SINGLE_PRODUCT":
+        return {
+            ...state,
+            isSingleLoading:false,
+            singleProduct:action.payload,
+        }   
+      case "SET_SINGLE_ERROR":
+        return {
+            ...state,
+            isSingleLoading:false,
+            isError:true
+
+        }   
     default:
         return state;
 }
