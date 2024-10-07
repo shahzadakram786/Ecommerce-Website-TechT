023feeb4cf9a7698bdc55a85e9678572d0935ae6
shahzadakram786 <alyakram786@gmail.com>
2024-10-07 20:43:45 +0500

    filter context

--- src/context/filterContext.js
+++ src/context/filterContext.js
@@ -1,34 +1,35 @@
-// import { createContext, useContext, useEffect, useReducer } from "react";
-// import { useProductContext } from "./productcontex";
-// import reducer from "../reducer/filterReducer";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { useProductContext } from "./productcontex";
+import reducer from "../reducer/filterReducer";
 
-// const FilterContext = createContext();
+const FilterContext = createContext();
 
-// export const FilterContextProvider = ({childern}) => {
+export const FilterContextProvider = ({ children }) => {
+    const { products =[] } = useProductContext();
 
-//     const {products} = useProductContext();
+    const initialState = {
+        filter_products: [],
+        all_products: []
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        // if (Array.isArray(products)) {
+            dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
+        // } else {
+            console.warn("Products is not an array:", products);
+        // }
+    }, [products]);
+
+    return <FilterContext.Provider value={{ ...state }}>
+        {children}
+    </FilterContext.Provider>;
+};
 
 
-//     const initialState = {
-//         filter_products:[],
-//         all_products:[]
-//     }
+//custome hooks
 
-//     const[state  , dispatch] = useReducer(reducer , initialState)
-
-//         useEffect(()=>{
-//             dispatch({type:"LOAD_FILTER_PRODUCTS", payload:products})
-//         },[])
-
-
-//     return <FilterContext.Provider value={{...state}}>
-//         {childern}
-//     </FilterContext.Provider>
-
-// }
-
-// //custome hooks
-
-// export const useFilterContext = () =>{
-//     return useContext(FilterContext)
-// }
+export const useFilterContext = () =>{
+    return useContext(FilterContext)
+}
